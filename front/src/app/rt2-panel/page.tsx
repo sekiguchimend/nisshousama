7a5f77f881811c4fcf304070a724121c46e35d1b
foxey <foxeyr1103@gmail.com>
2025-09-08 15:45:40 +0900

    RT-2パネルのフロント

--- front/src/app/rt2-panel/page.tsx
+++ front/src/app/rt2-panel/page.tsx
@@ -5,348 +5,9 @@ import { useRouter } from 'next/navigation';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { ArrowLeft } from "lucide-react";
+import { TodayCastData } from '@/types/cast';
+import { sampleCastData } from '@/data/castSampleData';
 
-// キャストデータの型定義
-interface todayCastData {
-  id: string;
-  name: string;
-  startTime: string;
-  endTime: string;
-  homeTime: string;
-  deliverPlace: string;
-  achieve: string;
-  finalCustomer: string;
-  nowCustomer: string;
-  inDriverMoving: string;
-  next1: string;
-  next2: string;
-  next3: string;
-  next4: string;
-  next5: string;
-  remark: string;
-  special: string;
-  ngPlace: string;
-  waitTime: string;
-}
-
-// サンプルデータ
-const sampleCastData: todayCastData[] = [
-  {
-    id: "001",
-    name: "美咲",
-    startTime: "18:00",
-    endTime: "2:00",
-    homeTime: "21:30",
-    deliverPlace: "銀座駅",
-    achieve: "3200",
-    finalCustomer: "六本木ヒルズ",
-    nowCustomer: "六本木ヒルズ",
-    inDriverMoving: "1:15",
-    next1: "22:30 渋谷",
-    next2: "0:15 新宿",
-    next3: "",
-    next4: "",
-    next5: "",
-    remark: "VIP",
-    special: "新規顧客対応",
-    ngPlace: "",
-    waitTime: ""
-  },
-  {
-    id: "002",
-    name: "さくら",
-    startTime: "19:30",
-    endTime: "3:00",
-    homeTime: "22:45",
-    deliverPlace: "新宿駅南口",
-    achieve: "2800",
-    finalCustomer: "新宿駅南口",
-    nowCustomer: "表参道駅",
-    inDriverMoving: "0:25",
-    next1: "23:00 表参道",
-    next2: "1:30 池袋",
-    next3: "2:45 品川",
-    next4: "",
-    next5: "",
-    remark: "Lady",
-    special: "",
-    ngPlace: "歌舞伎町周辺",
-    waitTime: ""
-  },
-  {
-    id: "003",
-    name: "まゆ",
-    startTime: "20:00",
-    endTime: "4:00",
-    homeTime: "23:20",
-    deliverPlace: "恵比寿駅",
-    achieve: "4500",
-    finalCustomer: "恵比寿駅",
-    nowCustomer: "赤坂見附",
-    inDriverMoving: "0:45",
-    next1: "0:30 麻布",
-    next2: "",
-    next3: "",
-    next4: "",
-    next5: "",
-    remark: "Girls",
-    special: "VIP対応必須",
-    ngPlace: "",
-    waitTime: ""
-  },
-  {
-    id: "004",
-    name: "りな",
-    startTime: "18:30",
-    endTime: "1:30",
-    homeTime: "20:15",
-    deliverPlace: "羽田空港",
-    achieve: "1800",
-    finalCustomer: "お台場駅",
-    nowCustomer: "",
-    inDriverMoving: "0:15",
-    next1: "21:00 お台場",
-    next2: "23:30 豊洲",
-    next3: "0:45 晴海",
-    next4: "",
-    next5: "",
-    remark: "新人",
-    special: "",
-    ngPlace: "",
-    waitTime: "15分"
-  },
-  {
-    id: "005",
-    name: "あいり",
-    startTime: "21:00",
-    endTime: "5:00",
-    homeTime: "1:10",
-    deliverPlace: "成田空港",
-    achieve: "5200",
-    finalCustomer: "成田空港",
-    nowCustomer: "東京駅",
-    inDriverMoving: "2:30",
-    next1: "2:00 大手町",
-    next2: "3:15 丸の内",
-    next3: "4:30 八重洲",
-    next4: "",
-    next5: "",
-    remark: "SUP",
-    special: "長距離対応",
-    ngPlace: "",
-    waitTime: ""
-  },
-  {
-    id: "006",
-    name: "ゆき",
-    startTime: "19:00",
-    endTime: "2:30",
-    homeTime: "22:00",
-    deliverPlace: "品川駅",
-    achieve: "3100",
-    finalCustomer: "品川駅",
-    nowCustomer: "五反田駅",
-    inDriverMoving: "0:40",
-    next1: "23:15 五反田",
-    next2: "1:00 目黒",
-    next3: "",
-    next4: "",
-    next5: "",
-    remark: "Lady",
-    special: "",
-    ngPlace: "大崎周辺",
-    waitTime: ""
-  },
-  {
-    id: "007",
-    name: "かな",
-    startTime: "20:30",
-    endTime: "3:30",
-    homeTime: "0:45",
-    deliverPlace: "浦安駅",
-    achieve: "2400",
-    finalCustomer: "浦安駅",
-    nowCustomer: "舞浜",
-    inDriverMoving: "1:20",
-    next1: "1:30 新浦安",
-    next2: "2:45 市川",
-    next3: "",
-    next4: "",
-    next5: "",
-    remark: "Girls",
-    special: "",
-    ngPlace: "",
-    waitTime: ""
-  },
-  {
-    id: "008",
-    name: "えみ",
-    startTime: "18:45",
-    endTime: "1:00",
-    homeTime: "21:30",
-    deliverPlace: "横浜駅",
-    achieve: "2700",
-    finalCustomer: "みなとみらい駅",
-    nowCustomer: "",
-    inDriverMoving: "0:20",
-    next1: "22:30 みなとみらい",
-    next2: "0:15 中華街",
-    next3: "",
-    next4: "",
-    next5: "",
-    remark: "VIP",
-    special: "新規エリア",
-    ngPlace: "",
-    waitTime: "10分"
-  },
-  {
-    id: "009",
-    name: "みお",
-    startTime: "22:00",
-    endTime: "6:00",
-    homeTime: "2:20",
-    deliverPlace: "千葉駅",
-    achieve: "3800",
-    finalCustomer: "千葉駅",
-    nowCustomer: "津田沼",
-    inDriverMoving: "0:50",
-    next1: "3:00 船橋",
-    next2: "4:30 柏",
-    next3: "5:15 松戸",
-    next4: "",
-    next5: "",
-    remark: "Lady",
-    special: "長時間対応",
-    ngPlace: "",
-    waitTime: ""
-  },
-  {
-    id: "010",
-    name: "なな",
-    startTime: "19:15",
-    endTime: "2:45",
-    homeTime: "23:10",
-    deliverPlace: "大宮駅",
-    achieve: "2900",
-    finalCustomer: "大宮駅",
-    nowCustomer: "浦和駅",
-    inDriverMoving: "0:55",
-    next1: "0:00 浦和",
-    next2: "1:30 川口",
-    next3: "",
-    next4: "",
-    next5: "",
-    remark: "Girls",
-    special: "",
-    ngPlace: "大宮周辺繁華街",
-    waitTime: ""
-  },
-  {
-    id: "011",
-    name: "ひな",
-    startTime: "21:30",
-    endTime: "4:30",
-    homeTime: "1:45",
-    deliverPlace: "立川駅",
-    achieve: "1900",
-    finalCustomer: "立川駅",
-    nowCustomer: "八王子",
-    inDriverMoving: "1:05",
-    next1: "2:30 町田",
-    next2: "3:45 多摩センター",
-    next3: "",
-    next4: "",
-    next5: "",
-    remark: "新人",
-    special: "研修中",
-    ngPlace: "",
-    waitTime: ""
-  },
-  {
-    id: "012",
-    name: "あや",
-    startTime: "20:15",
-    endTime: "3:15",
-    homeTime: "0:30",
-    deliverPlace: "吉祥寺駅",
-    achieve: "3600",
-    finalCustomer: "吉祥寺駅",
-    nowCustomer: "三鷹",
-    inDriverMoving: "0:35",
-    next1: "1:15 調布",
-    next2: "2:30 府中",
-    next3: "",
-    next4: "",
-    next5: "",
-    remark: "SUP",
-    special: "指名多数",
-    ngPlace: "",
-    waitTime: ""
-  },
-  {
-    id: "013",
-    name: "れい",
-    startTime: "23:00",
-    endTime: "7:00",
-    homeTime: "3:15",
-    deliverPlace: "武蔵小杉駅",
-    achieve: "4100",
-    finalCustomer: "武蔵小杉駅",
-    nowCustomer: "川崎駅",
-    inDriverMoving: "1:10",
-    next1: "4:00 川崎",
-    next2: "5:30 横浜",
-    next3: "6:15 鶴見",
-    next4: "",
-    next5: "",
-    remark: "VIP",
-    special: "深夜専門",
-    ngPlace: "",
-    waitTime: ""
-  },
-  {
-    id: "014",
-    name: "みく",
-    startTime: "18:15",
-    endTime: "0:15",
-    homeTime: "20:50",
-    deliverPlace: "秋葉原駅",
-    achieve: "2300",
-    finalCustomer: "秋葉原駅",
-    nowCustomer: "御茶ノ水",
-    inDriverMoving: "0:25",
-    next1: "21:45 神田",
-    next2: "23:00 東京",
-    next3: "",
-    next4: "",
-    next5: "",
-    remark: "Girls",
-    special: "",
-    ngPlace: "秋葉原周辺",
-    waitTime: ""
-  },
-  {
-    id: "015",
-    name: "こはる",
-    startTime: "22:30",
-    endTime: "5:30",
-    homeTime: "2:40",
-    deliverPlace: "池袋駅",
-    achieve: "3300",
-    finalCustomer: "新宿駅",
-    nowCustomer: "",
-    inDriverMoving: "0:45",
-    next1: "3:30 新宿",
-    next2: "4:45 渋谷",
-    next3: "",
-    next4: "",
-    next5: "",
-    remark: "Lady",
-    special: "夜間専門",
-    ngPlace: "",
-    waitTime: "20分"
-  }
-];
 
 
 
@@ -362,15 +23,6 @@ export default function RT2Panel() {
     return () => clearInterval(timer);
   }, []);
 
-  const formatDate = (date: Date) => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    const weekdays = ['日', '月', '火', '水', '木', '金', '土'];
-    const weekday = weekdays[date.getDay()];
-    
-    return `${year}/${month}/${day}(${weekday})`;
-  };
 
   const formatTime = (date: Date) => {
     const hours = String(date.getHours()).padStart(2, '0');
@@ -436,33 +88,37 @@ export default function RT2Panel() {
       <Card>
         <CardContent className="p-0">
           <div className="overflow-x-auto">
-            <table className="w-full text-xs table-fixed">
+            <table className="w-full text-[10px] table-fixed">
               <colgroup>
-                <col style={{width: '80px'}} />
-                <col style={{width: '80px'}} />
+                <col style={{width: '55px'}} />
+                <col style={{width: '50px'}} />
+                <col style={{width: '40px'}} />
+                <col style={{width: '40px'}} />
+                <col style={{width: '40px'}} />
+                <col style={{width: '70px'}} />
+                <col style={{width: '140px'}} />
+                <col style={{width: '24px'}} />
+                <col style={{width: '140px'}} />
+                <col style={{width: '24px'}} />
+                <col style={{width: '140px'}} />
+                <col style={{width: '24px'}} />
+                <col style={{width: '140px'}} />
+                <col style={{width: '24px'}} />
+                <col style={{width: '140px'}} />
+                <col style={{width: '24px'}} />
+                <col style={{width: '140px'}} />
+                <col style={{width: '24px'}} />
+                <col style={{width: '140px'}} />
+                <col style={{width: '24px'}} />
+                <col style={{width: '140px'}} />
+                <col style={{width: '24px'}} />
+                <col style={{width: '70px'}} />
                 <col style={{width: '60px'}} />
+                <col style={{width: '70px'}} />
                 <col style={{width: '60px'}} />
-                <col style={{width: '60px'}} />
-                <col style={{width: '100px'}} />
-                <col style={{width: '150px'}} />
-                <col style={{width: '32px'}} />
-                <col style={{width: '150px'}} />
-                <col style={{width: '32px'}} />
-                <col style={{width: '150px'}} />
-                <col style={{width: '32px'}} />
-                <col style={{width: '150px'}} />
-                <col style={{width: '32px'}} />
-                <col style={{width: '150px'}} />
-                <col style={{width: '32px'}} />
-                <col style={{width: '150px'}} />
-                <col style={{width: '32px'}} />
-                <col style={{width: '150px'}} />
-                <col style={{width: '32px'}} />
-                <col style={{width: '150px'}} />
-                <col style={{width: '32px'}} />
-                <col style={{width: '100px'}} />
-                <col style={{width: '100px'}} />
-                <col style={{width: '120px'}} />
+                <col style={{width: '40px'}} />
+                <col style={{width: '30px'}} />
+                <col style={{width: '130px'}} />
               </colgroup>
               <thead>
                 <tr className="bg-white">
@@ -489,7 +145,11 @@ export default function RT2Panel() {
                   <th className="border border-gray-300 px-2 py-1 text-center">予約5</th>
                   <th className="border border-gray-300 px-1 py-1 text-center"></th>
                   <th className="border border-gray-300 px-2 py-1 text-center">出席備考</th>
+                  <th className="border border-gray-300 px-2 py-1 text-center">待ち時間</th>
                   <th className="border border-gray-300 px-2 py-1 text-center">特記事項</th>
+                  <th className="border border-gray-300 px-2 py-1 text-center">web状態</th>
+                  <th className="border border-gray-300 px-2 py-1 text-center">mode</th>
+                  <th className="border border-gray-300 px-2 py-1 text-center">順位</th>
                   <th className="border border-gray-300 px-2 py-1 text-center">ホステスNG確認</th>
                 </tr>
               </thead>
@@ -533,7 +193,7 @@ export default function RT2Panel() {
                     {/* 最終接客ボタン */}
                     <td className="border border-gray-300 p-0 text-center">
                       {cast.finalCustomer && (
-                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-xs border-0 flex items-center justify-center rounded-none rounded-none">＜</button>
+                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-[8px] border-0 flex items-center justify-center rounded-none rounded-none">＜</button>
                       )}
                     </td>
                     {/* 接客中 */}
@@ -546,7 +206,7 @@ export default function RT2Panel() {
                     {/* 接客中ボタン */}
                     <td className="border border-gray-300 p-0 text-center">
                       {cast.nowCustomer && (
-                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-xs border-0 flex items-center justify-center rounded-none">＜</button>
+                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-[8px] border-0 flex items-center justify-center rounded-none">＜</button>
                       )}
                     </td>
                     {/* INドライバー稼働中 */}
@@ -561,7 +221,7 @@ export default function RT2Panel() {
                     {/* INドライバー稼働中ボタン */}
                     <td className="border border-gray-300 p-0 text-center">
                       {(cast.nowCustomer || cast.deliverPlace) && (
-                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-xs border-0 flex items-center justify-center rounded-none">＜</button>
+                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-[8px] border-0 flex items-center justify-center rounded-none">＜</button>
                       )}
                     </td>
                     {/* 予約1 */}
@@ -576,7 +236,7 @@ export default function RT2Panel() {
                     {/* 予約1ボタン */}
                     <td className="border border-gray-300 p-0 text-center">
                       {cast.next1 && (
-                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-xs border-0 flex items-center justify-center rounded-none">＜</button>
+                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-[8px] border-0 flex items-center justify-center rounded-none">＜</button>
                       )}
                     </td>
                     {/* 予約2 */}
@@ -591,7 +251,7 @@ export default function RT2Panel() {
                     {/* 予約2ボタン */}
                     <td className="border border-gray-300 p-0 text-center">
                       {cast.next2 && (
-                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-xs border-0 flex items-center justify-center rounded-none">＜</button>
+                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-[8px] border-0 flex items-center justify-center rounded-none">＜</button>
                       )}
                     </td>
                     {/* 予約3 */}
@@ -606,7 +266,7 @@ export default function RT2Panel() {
                     {/* 予約3ボタン */}
                     <td className="border border-gray-300 p-0 text-center">
                       {cast.next3 && (
-                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-xs border-0 flex items-center justify-center rounded-none">＜</button>
+                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-[8px] border-0 flex items-center justify-center rounded-none">＜</button>
                       )}
                     </td>
                     {/* 予約4 */}
@@ -621,7 +281,7 @@ export default function RT2Panel() {
                     {/* 予約4ボタン */}
                     <td className="border border-gray-300 p-0 text-center">
                       {cast.next4 && (
-                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-xs border-0 flex items-center justify-center rounded-none">＜</button>
+                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-[8px] border-0 flex items-center justify-center rounded-none">＜</button>
                       )}
                     </td>
                     {/* 予約5 */}
@@ -636,16 +296,32 @@ export default function RT2Panel() {
                     {/* 予約5ボタン */}
                     <td className="border border-gray-300 p-0 text-center">
                       {cast.next5 && (
-                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-xs border-0 flex items-center justify-center rounded-none">＜</button>
+                        <button className="w-full h-full bg-green-700 hover:bg-green-800 text-white text-[8px] border-0 flex items-center justify-center rounded-none">＜</button>
                       )}
                     </td>
                     {/* 出席備考 */}
                     <td className="border border-gray-300 px-2 py-1 text-center">
                       <div className="text-black">{cast.special}</div>
                     </td>
+                    {/* 待ち時間 */}
+                    <td className="border border-gray-300 px-2 py-1 text-center">
+                      <div className="text-black">{cast.waitTime}</div>
+                    </td>
                     {/* 特記事項 */}
                     <td className="border border-gray-300 px-2 py-1 text-center">
                       <div className="text-black">{cast.remark}</div>
+                    </td>
+                    {/* web状態 */}
+                    <td className="border border-gray-300 px-2 py-1 text-center">
+                      <div className={cast.webStatus === "オンライン" ? "text-green-600" : "text-red-600"}>{cast.webStatus}</div>
+                    </td>
+                    {/* mode */}
+                    <td className="border border-gray-300 px-2 py-1 text-center">
+                      <div className={cast.mode === "自動" ? "text-blue-600" : "text-orange-600"}>{cast.mode}</div>
+                    </td>
+                    {/* 順位 */}
+                    <td className="border border-gray-300 px-2 py-1 text-center">
+                      <div className="text-black font-bold">{cast.ranking}</div>
                     </td>
                     {/* ホステスNG確認 */}
                     <td className="border border-gray-300 px-2 py-1 text-center">
---
+++ front/src/types/cast.ts
@@ -0,0 +1,25 @@
+// キャストデータの型定義
+export interface TodayCastData {
+  id: string;
+  name: string;
+  startTime: string;
+  endTime: string;
+  homeTime: string;
+  deliverPlace: string;
+  achieve: string;
+  finalCustomer: string;
+  nowCustomer: string;
+  inDriverMoving: string;
+  next1: string;
+  next2: string;
+  next3: string;
+  next4: string;
+  next5: string;
+  remark: string;
+  special: string;
+  ngPlace: string;
+  waitTime: string;
+  webStatus: string;
+  mode: string;
+  ranking: string;
+}
